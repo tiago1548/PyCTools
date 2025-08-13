3713e72b7d983f8ad5596e1079027618e2b3be05
Shahm Najeeb <Nirt_12023@outlook.com>
2025-08-13 15:22:42 +0300

    Removal of examples to be in documented in Wiki
    Update .gitignore: ensure consistency in ignoring pyCTools/bin directory
    Updated README to reflect the changes

--- example/hwrng_example.py
+++
@@ -1,9 +0,0 @@
-# Optional CLI usage
-from pyCTools.hwrng import get_hardware_random_bytes
-
-if __name__ == '__main__':
-    try:
-        rb = get_hardware_random_bytes(256)
-        print("Random bytes:", rb.hex())
-    except Exception as e:
-        print("Error:", e)
